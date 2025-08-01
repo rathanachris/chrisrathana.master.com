7900f438541ed6e20b57eb8ef4e56d9a2a9cc69d
rathanachris <rathanachris239@gmail.com>
2025-08-01 21:21:57 +0700

    Create index.js
    Signed-off-by: rathanachris <rathanachris239@gmail.com>

---
+++ Package.json/index.js
@@ -0,0 +1,11 @@
+{
+  "name": "my-first-package",
+  "version": "1.0.0",
+  "description": "My awesome service",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "author": "your-name",
+  "license": "MIT"
+}
