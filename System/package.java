443695c5525ecff6f693e7e88dde6f8f1c200bf8
chris rathana <rathanachris239@gmail.com>
2025-10-23 14:18:26 +0700

    Create package.java
    Signed-off-by: chris rathana <rathanachris239@gmail.com>

---
+++ System/package.java
@@ -0,0 +1,13 @@
+public class com.master.java.net{
+
+     String token ="..........."
+     System.getenv("API_TOKEN");
+     
+     HttpsURLConnection conn =
+      (HttpsURLConnection)
+      
+       url.openConnection();
+       
+conn.setRequestProperty("Authorizati + token); on", "Bearer 11
+
+}
