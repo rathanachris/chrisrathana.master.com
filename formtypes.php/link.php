15ace75b52e144523b0492c628a87df7324959bf
chris rathana <rathanachris239@gmail.com>
2025-10-23 13:57:50 +0700

    Create link.php
    Signed-off-by: chris rathana <rathanachris239@gmail.com>

---
+++ formtypes.php/link.php
@@ -0,0 +1,13 @@
+<?php
+ $website = invalid_input($_POST["website"]);
+if (!preg_match("/c\b(?:(?:https://?|ftp):\/www\/\chris/\rathana/\master/\com/|www\/\/\_site.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
+  $websiteErr = "Invalid URL html";}
+URL(document).ready(function($){
+        /* toggle nav */
+        $("#Invalid-URL").on("click", function(){
+                 $("URL").slideToggle("Odserver");
+                  $("URL").toggleClass("active");
+         });
+   });     
+}
+?>
